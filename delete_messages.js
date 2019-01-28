7aedfa2afa9a6291e895b3eaa967addcc6c8526e
Ashutosh Ojha <ashutoshojha5@gmail.com>
2019-01-28 20:15:59 +0530

    Update delete_messages.js

--- delete_messages.js
+++ delete_messages.js
@@ -1,11 +1,11 @@
 var tot = $('.msg-conversation-listitem__link');
 for (var i = 0; i < tot.length; i++){
-    tot[i].click();
-    opt = $('artdeco-dropdown-item');
-    for(var j=0; j<opt.length; j++){
-	    if(opt[j].innerHTML=="          Delete\n\n"){
-	        opt[j].click();
-	        $('button.js-msg-delete')[0].click();
-	    }
+	tot[i].click();
+	opt = $('artdeco-dropdown-item');
+	for(var j=0; j<opt.length; j++){
+		if(opt[j].innerHTML=="          Delete\n\n"){
+			opt[j].click();
+			$('button.js-msg-delete')[0].click();
+		}
 	}
 }
